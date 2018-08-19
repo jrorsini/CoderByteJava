33b3d24f226f434d909a20a363df86b126dad527
Jean-Roger <jeanroger.orsini@gmail.com>
2018-08-20 03:38:37 +0900

    Now I know when to use method.

--- CoderByte.java
+++ CoderByte.java
@@ -5,24 +5,11 @@ public class CoderByte {
     int levelCompleted = 5;
     int bonus = 100;
 
-    calculateScore();
-
-    score = 10000;
-    levelCompleted = 8;
-    bonus = 200;
-
-    if (gameOver) {
-      int finalScore = score + (levelCompleted * bonus);
-      System.out.println("Your final score was " + finalScore);
-    }
+    calculateScore(true, 800, 5, 100);
+    calculateScore(true, 10000, 8, 200);
   }
 
-  public static void calculateScore() {
-    boolean gameOver = true;
-    int score = 800;
-    int levelCompleted = 5;
-    int bonus = 100;
-
+  public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
     if (gameOver) {
       int finalScore = score + (levelCompleted + bonus);
       finalScore += 1000;
