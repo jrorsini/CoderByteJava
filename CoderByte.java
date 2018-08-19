807524a581cd4174f177bf07afe05c37b22957c7
Jean-Roger <jeanroger.orsini@gmail.com>
2018-08-20 03:45:45 +0900

    minor.

--- CoderByte.java
+++ CoderByte.java
@@ -1,19 +1,10 @@
 public class CoderByte {
+
   public static void main(String[] args) {
-    boolean gameOver = true;
-    int score = 800;
-    int levelCompleted = 5;
-    int bonus = 100;
+    String numberAsString = "2018";
+    System.out.println("numberAsString= " + numberAsString);
 
-    calculateScore(true, 800, 5, 100);
-    calculateScore(true, 10000, 8, 200);
-  }
-
-  public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-    if (gameOver) {
-      int finalScore = score + (levelCompleted + bonus);
-      finalScore += 1000;
-      System.out.println("Your final score was " + finalScore);
-    }
+    int number = Integer.parseInt(numberAsString);
+    System.out.println("number= " + number);
   }
 }
