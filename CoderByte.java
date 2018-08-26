0e2ed7de5cd21620332f646077239c364225a52e
Jean-Roger <jeanroger.orsini@gmail.com>
2018-08-26 12:17:33 +0900

    Finally got a better grasp of the Scanner native class:

--- CoderByte.java
+++ CoderByte.java
@@ -1,16 +1,6 @@
 public class CoderByte {
 
   public static void main(String[] args) {
-    String numberAsString = "2018";
-    System.out.println("numberAsString= " + numberAsString);
 
-    int number = Integer.parseInt(numberAsString);
-    System.out.println("number= " + number);
-
-    numberAsString += 1;
-    number += 1;
-
-    System.out.println("numberAsString= " + numberAsString);
-    System.out.println("number= " + number);
   }
 }
--- Hello.java
+++ Hello.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Hello {
   public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
 
-    String myFirstNumber = "Jean-Roger";
+    System.out.println("Enter your year of birth: ");
+    int yearOfBirth = read.nextInt();
+    read.nextLine();
 
-    System.out.println("Hello " + myFirstNumber + "!");
+    System.out.println("Enter your name, please: ");
+    String name = read.nextLine();
+    int age = 2018 - yearOfBirth;
+
+    System.out.println("Your name is " + name + ", and you are " + age + " years old");
+
+    read.close();
   }
 }
