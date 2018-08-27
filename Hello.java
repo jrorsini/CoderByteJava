bc739cda7bb1aacec8e7e03e6aca5ae505a8bc2f
Jean-Roger <jeanroger.orsini@gmail.com>
2018-08-27 13:58:02 +0900

    minor.

--- Hello.java
+++ Hello.java
@@ -2,8 +2,16 @@ import java.util.Scanner;
 
 public class Hello {
   public static void main(String[] args) {
-    String randomString = "I'm just a random string";
-    String gotToQuote = "\"test\"";
-    System.out.println(gotToQuote);
+    // StringBuilder str = new StringBuilder("fun times!");
+    String str = "fun times!";
+    StringBuilder result = new StringBuilder();
+    char[] strChars = str.toLowerCase().toCharArray();
+    System.out.println(strChars);
+    char[] replacements = { 'b', 'c', 'd', 'E', 'f', 'g', 'h', 'I', 'j', 'k', 'l', 'm', 'n', 'O', 'p', 'q', 'r', 's',
+        't', 'U', 'v', 'w', 'x', 'y', 'z', 'A' };
+    for (int i = 0; i < str.length(); i++) {
+      result.append(strChars[i] >= 'a' && strChars[i] <= 'z' ? replacements[strChars[i] - 97] : strChars[i]);
+    }
+    System.out.println(result.toString());
   }
 }
