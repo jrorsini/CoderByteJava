6065b9b8340a8bfd9a3c8a55d1f9426822302b1d
Jean-Roger <jeanroger.orsini@gmail.com>
2018-08-27 11:40:42 +0900

    Testing strings.

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Hello {
   public static void main(String[] args) {
-
+    String randomString = "I'm just a random string";
+    String gotToQuote = "\"test\"";
+    System.out.println(gotToQuote);
   }
 }
