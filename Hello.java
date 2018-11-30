26bbc7e9cb23c3e754b2da4a5fdcfc6c8f88dfae
Jean-Roger <jeanroger.orsini@gmail.com>
2018-11-30 12:33:35 -0800

    minor.

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String[] args) {
-    int age;
+    byte age;
     age = 25;
     String name = "Jean-Roger";
     System.out.println("Hello World " + name + " you are " + age);
