5c0630734643509ca068c78343e6d754a4ed94e3
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-01 14:01:44 -0800

    minor.

--- Hello.java
+++ Hello.java
@@ -11,13 +11,21 @@ public class Hello {
 
   // return(num===0||num===1)?num:num*FirstFactorial(num-1)
 
-  static Int firstFactorial(int num) {
-    return num > 0 ? 0 : 1;
+  static int SimpleAdding(int num) {
+    int res = 0;
+    for (int i = 1; i <= num; i++) {
+      res += i;
+    }
+    return res;
+  }
+
+  static int firstFactorial(int num) {
+    return num == 1 ? num : num * firstFactorial(num - 1);
   }
 
   public static void main(String[] args) {
     String name = "Jean-Roger";
     System.out.println("Hello " + name + ", " + isLegal(19));
-    System.out.println(firstFactorial(2));
+    System.out.println(SimpleAdding(12));
   }
 }
