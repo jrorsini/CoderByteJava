0edcf839f3dea4e032a1e404f134a431cdc11bad
Jean-Roger <jeanroger.orsini@gmail.com>
2018-11-30 22:36:16 -0800

    minor.

--- Hello.java
+++ Hello.java
@@ -1,17 +1,18 @@
 public class Hello {
-  public static void main(String[] args) {
-    String name = "Jean-Roger";
-
-    System.out.println("Hello " + name + ", " + isLegal(25));
-
-  }
+  int x = 5;
 
   static String isLegal(int age) {
-    int legalAge2Drink = 21;
-    if (age > legalAge2Drink) {
+    if (age > 21) {
       return "here in America, you are legal to drink!";
     } else {
       return "you are " + age + " years old, you are not legal to drink here!";
     }
+  }
+
+  public static void main(String[] args) {
+    String name = "Jean-Roger";
+    System.out.println("Hello " + name + ", " + isLegal(19));
+    Hello myObj = new Hello();
+    System.out.println(myObj.x);
   }
 }
