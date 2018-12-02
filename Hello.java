219e49babe4a9cc60f0c0d7ef0fe3e6853317d15
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-02 14:46:19 -0800

    minor.

--- Hello.java
+++ Hello.java
@@ -24,12 +24,7 @@ public class Hello {
    */
 
   static String CheckNums(int num1, int num2) {
-    if (num2 > num1) {
-      return "true";
-    } else if (num2 == num1) {
-      return "false";
-    }
-    return "-1";
+    return num1 == num2 ? "-1" : String.valueOf(num2 > num1);
   }
 
   static String letterCapitalize(String str) {
