1c89fc50a70e94068727795e942779e85bbcd93c
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-13 10:27:40 -0800

    minor.

--- Hello.java
+++ Hello.java
@@ -10,23 +10,20 @@ public class Hello {
     this.name = "Orsini";
   }
 
-  public static int SecondGreatLow(int[] arr) {
+  public static String SecondGreatLow(int[] arr) {
     Arrays.sort(arr);
-    for (int i = 0; i < arr.length; i++) {
-      System.out.println(arr[i]);
-    }
     if (arr.length == 0) {
-      return String.valueOf(arr[0] + " " + arr[1]);
+      return arr[0] + " " + arr[1];
     } else if (arr.length == 3) {
-      return String.valueOf(arr[1] + " " + arr[1]);
+      return arr[1] + " " + arr[1];
     } else {
-      return String.valueOf(arr[1] + " " + arr[arr.length - 2]);
+      return arr[1] + " " + arr[arr.length - 2];
     }
   }
 
   public static void main(String[] args) {
     Hello obj = new Hello();
-    SecondGreatLow(new int[] { 5, 5, 1, 2, 3 });
+    System.out.println(SecondGreatLow(new int[] { 5, 5, 1, 2, 3 }));
     System.out.println(obj.name);
   }
 }
