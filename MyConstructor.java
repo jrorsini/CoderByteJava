bfc19e93d0ed2d5480140f4863f1c82788529062
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-13 23:42:59 -0800

    minor.

--- MyConstructor.java
+++ MyConstructor.java
@@ -1,14 +1,25 @@
 public class MyConstructor {
   String name;
+  int empId;
+  String empName;
 
   public MyConstructor() {
     System.out.println("This is a no argument constructor");
-    name = "BeginnersBook.com";
+    this.name = "BeginnersBook.com";
+  }
+
+  MyConstructor(int id, String name) {
+    this.empId = id;
+    this.empName = name;
+  }
+
+  void info() {
+    System.out.println("Id: " + empId + " Name: " + empName);
   }
 
   public static void main(String[] args) {
     MyConstructor test = new MyConstructor();
-    new MyConstructor();
+    // new MyConstructor();
     System.out.println(test.name);
   }
 }
