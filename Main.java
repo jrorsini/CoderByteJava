b0af961aed10abd82ab4354a86e35b2a58f8734c
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-18 12:20:42 -0800

    minor.

--- Main.java
+++ Main.java
@@ -7,7 +7,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+
+  enum KeyType {
+    APPLE, BANANA, PEAR;
+  }
+
   public static void main(String[] args) {
-    Map<K, V> dataMap = new HashMap<K, V>();
+    Map<KeyType, Integer> dataMap = new HashMap<KeyType, Integer>();
+    dataMap.put(KeyType.APPLE, 0);
+    dataMap.put(KeyType.BANANA, 0);
+    dataMap.put(KeyType.PEAR, 0);
+    System.out.println(dataMap.keySet());
   }
 }
