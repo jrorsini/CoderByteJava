dd540dcc0f11108785dab9a21c6288201a42ef46
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-20 22:04:27 -0800

    minor.

---
+++ UsingMethods.java
@@ -0,0 +1,16 @@
+/**
+ * UsingMethods
+ */
+public class UsingMethods {
+
+  public static void getSum(int num1, int num2) {
+    int result = num1 + num2;
+    System.out.println(result);
+  }
+
+  public static void main(String[] args) {
+    int x = 20;
+    int y = 50;
+    getSum(x, y);
+  }
+}
