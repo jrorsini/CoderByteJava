8db1d60f143f57502a2f87cc3e9ffe56f6c62792
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-21 18:37:25 -0800

    Variables.

---
+++ Variables.java
@@ -0,0 +1,20 @@
+/**
+ * Variables
+ */
+public class Variables {
+  public static void main(String[] args) {
+    // data type
+    // name
+    // assigner
+    // value
+    int number = 1000000;
+    short shortNum = 34234;
+    float pi = 3.14f;
+    double PI = 3.14;
+    // long version data type int
+    long veryLongNumber = 14223;
+    byte id = 1;
+
+    String name = "Jean-Roger";
+  }
+}
