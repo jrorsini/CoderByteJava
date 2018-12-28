1b48ff348c2b9d2fdaeaee549f1b68870eb72819
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-28 14:33:03 -0800

    minor.

--- javaBeginner/w3school/MyClass.java
+++ javaBeginner/w3school/MyClass.java
@@ -1,13 +1,17 @@
-package javaBeginner.w3school;
-
 /**
  * MyClass
  */
 public class MyClass {
   int x = 5;
+  int y = 3;
 
   public static void main(String[] args) {
-    MyClass myObj = new MyClass();
-    System.out.println(myObj.x);
+    MyClass myObj1 = new MyClass();
+    MyClass myObj2 = new MyClass();
+    MyClass myObj3 = new MyClass();
+    myObj3.x = 40;
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
+    System.out.println(myObj3.x);
   }
 }
---
+++ javaBeginner/w3school/OtherClass.java
@@ -0,0 +1,10 @@
+/**
+ * OtherClass
+ */
+public class OtherClass {
+
+  public static void main(String[] args) {
+    MyClass myObj = new MyClass();
+    System.out.println(myObj.x);
+  }
+}
