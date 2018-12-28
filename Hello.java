55df8e2c550aee0403badadc6d1099d219155e24
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-28 13:48:14 -0800

    minor.

--- Hello.java
+++ Hello.java
@@ -350,17 +350,14 @@ public class Hello {
     return "true";
   }
 
-  public static String HammingDistance(String[] strArr) {
+  public static int HammingDistance(String[] strArr) {
+    int ham = 0;
+    for (int i = 0; i < strArr[0].length(); i++) {
+      if (strArr[0].split("")[i].equals(strArr[1].split("")[i]) == false)
+        ham++;
 
-    // code goes here
-    /*
-     * Note: In Java the return type of a function and the parameter types being
-     * passed are defined, so this return call must match the return type of the
-     * function. You are free to modify the return type.
-     */
-
-    return strArr[0];
-
+    }
+    return ham;
   }
 
   public static void main(String[] args) {
