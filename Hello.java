65e1f5b42942fe87d22e8206788c62dd3de3d34a
Jean-Roger <jeanroger.orsini@gmail.com>
2018-12-29 19:45:10 -0800

    minor.

--- Hello.java
+++ Hello.java
@@ -363,7 +363,10 @@ public class Hello {
   public static String RectangleArea(String[] strArr) {
     int length;
     int width;
-    System.out.println(new StringBuilder(strArr[0].split(" ")[0]));
+    StringBuilder x = new StringBuilder(strArr[1].split(" ")[0]);
+    StringBuilder y = new StringBuilder(strArr[1].split(" ")[1]);
+    System.out.println(Integer.parseInt(String.valueOf(x.replace(0, 1, ""))));
+    System.out.println(Integer.parseInt(String.valueOf(y.replace(y.length() - 1, y.length(), ""))));
 
     return strArr[0];
   }
